fc5c4c7a57ee2508ba8136546e9acf77c63809bf
pragatishidodkar <pragatishidodkar@gmail.com>
2026-03-15 17:40:10 +0530

    Add movie objects for 'The Dark Knight' and 'Inception'

--- script.js
+++ script.js
@@ -15,3 +15,22 @@ ${createStars(index)}
 
 </div>
 `;
+{
+title:"The Dark Knight",
+genre:"Action",
+year:"2008",
+poster:"https://image.tmdb.org/t/p/w500/qJ2tW6WMUDux911r6m7haRef0WH.jpg",
+description:"Batman faces the Joker in Gotham City.",
+trailer:"https://www.youtube.com/embed/EXeTwQWrcwY",
+rating:9
+},
+
+{
+title:"Inception",
+genre:"Sci-Fi",
+year:"2010",
+poster:"https://image.tmdb.org/t/p/w500/qmDpIHrmpJINaRKAfWQfftjCdyi.jpg",
+description:"A thief enters dreams to steal secrets.",
+trailer:"https://www.youtube.com/embed/YoHD9XEInc0",
+rating:8
+},
