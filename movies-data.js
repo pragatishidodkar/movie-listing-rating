d0ea70af9e55deb1190c05b8c392026a9396e3b4
pragatishidodkar <pragatishidodkar@gmail.com>
2026-03-15 18:21:42 +0530

    Create movies-data.js

---
+++ movies-data.js
@@ -0,0 +1,59 @@
+const movieDatabase = {
+trending: [
+{
+title: "The Dark Knight",
+year: 2008,
+genre: "Action",
+rating: 9.0,
+poster: "https://image.tmdb.org/t/p/w500/qJ2tW6WMUDux911r6m7haRef0WH.jpg"
+},
+{
+title: "Inception",
+year: 2010,
+genre: "Sci-Fi",
+rating: 8.8,
+poster: "https://image.tmdb.org/t/p/w500/qmDpIHrmpJINaRKAfWQfftjCdyi.jpg"
+},
+{
+title: "Interstellar",
+year: 2014,
+genre: "Sci-Fi",
+rating: 8.6,
+poster: "https://image.tmdb.org/t/p/w500/rAiYTfKGqDCRIIqo664sY9XZIvQ.jpg"
+}
+],
+
+topRated: [
+{
+title: "The Shawshank Redemption",
+year: 1994,
+genre: "Drama",
+rating: 9.3,
+poster: "https://image.tmdb.org/t/p/w500/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg"
+},
+{
+title: "The Godfather",
+year: 1972,
+genre: "Crime",
+rating: 9.2,
+poster: "https://image.tmdb.org/t/p/w500/3bhkrj58Vtu7enYsRolD1fZdja1.jpg"
+}
+],
+
+newReleases: [
+{
+title: "Dune: Part Two",
+year: 2024,
+genre: "Sci-Fi",
+rating: 8.7,
+poster: "https://image.tmdb.org/t/p/w500/1pdfLvkbY9ohJlCjQH2CZjjYVvJ.jpg"
+},
+{
+title: "Oppenheimer",
+year: 2023,
+genre: "Biography",
+rating: 8.5,
+poster: "https://image.tmdb.org/t/p/w500/8GxvVqJ7S3KqkZ0kz1jF5rQ1JzO.jpg"
+}
+]
+};
